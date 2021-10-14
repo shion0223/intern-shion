7479e48f7bf80ff7ba91c0cea9420e4a4d4e629d
Shion Endo <>
2021-10-14 18:28:13 +0900

    no message

--- manner-app/src/app/app.module.ts
+++ manner-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './pages/top/top.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
