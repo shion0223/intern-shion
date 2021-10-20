1e6f8c44d47369da6e740a47bb8092ceffaeb478
Shion Endo <>
2021-10-20 18:24:32 +0900

    app-routing.module.tsにQuestionComponentをインポート routes配列にpathとcomponentを追加

--- manner-app/src/app/app-routing.module.ts
+++ manner-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './pages/question/question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'question', component: QuestionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
